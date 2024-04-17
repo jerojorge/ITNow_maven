5a416633a50604ba194d6e4ba085f7bfda0be077
jerojorge <99198160+jerojorge@users.noreply.github.com>
2024-04-17 10:59:05 +0200

    Add files via upload

---
+++ src/main/java/com/jero/mvfirst/MvfirstApplication.java
@@ -0,0 +1,13 @@
+package com.jero.mvfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvfirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvfirstApplication.class, args);
+	}
+
+}
